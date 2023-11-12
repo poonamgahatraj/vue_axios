135532626022164823c69b9bcaf719fe20a866df
Poonam Gahatraj <poonamgtrz@gmail.com>
2023-11-12 14:19:10 +0545

    newproject_3

---
+++ newproject_3/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import employee from './components/employee.vue';
+</script>
+
+<template>
+  <employee/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ newproject_3/src/components/employee.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>Hello</h1>
+</template>
+
+<script>
+import vue from 'vue';
+import axios from 'axios';
+import vueaxios from 'vue-Axios';
+vue.use(vueaxios,axios)
+
+export default{
+    data(){
+        return{
+
+        }
+    },
+
+    mounted(){
+        vue.axios.get('https://api.publicapis.org/entries')
+        .then((response)=>{
+
+            console.log(response.data)
+        })
+    }
+}
+</script>
